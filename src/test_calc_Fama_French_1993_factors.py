942b0a970464a7d1a48f011dbb59f972fe77eb9c
Jeremy Bejarano <jmbejara@gmail.com>
2024-02-06 18:48:42 -0500

    Update unit tests for new data

--- src/test_calc_Fama_French_1993_factors.py
+++ src/test_calc_Fama_French_1993_factors.py
@@ -31,15 +31,15 @@ def test_calc_book_equity_and_years_in_compustat():
     comp = calc_book_equity_and_years_in_compustat(comp)
     
     expected = """
-        gvkey   datadate     be
-        0  001000 1961-12-31    NaN
-        1  001000 1962-12-31    NaN
-        2  001000 1963-12-31  0.561
-        3  001000 1964-12-31  0.627
-        4  001000 1965-12-31  0.491
+        gvkey   datadate    be
+        001000 1961-12-31   NaN
+        001000 1962-12-31   NaN
+        001000 1963-12-31 0.561
+        001000 1964-12-31 0.627
+        001000 1965-12-31 0.491
     """
-    output = comp[["gvkey", "datadate", "be"]].head().to_string()
-
+    output = comp[["gvkey", "datadate", "be"]].head().to_string(index=False)
+    # print(output)
     assert output.replace(" ", "").replace("\n", "") == expected.replace(" ", "").replace("\n", "")
 
 def test_subset_CRSP_to_common_stock_and_exchanges():
@@ -47,7 +47,7 @@ def test_subset_CRSP_to_common_stock_and_exchanges():
     crsp = subset_CRSP_to_common_stock_and_exchanges(crsp)
     
     output = crsp.loc[crsp["jdate"] <= "2022", :].shape
-    expected = (3348587, 16)
+    expected = (3348395, 16)
     
     assert output== expected
 
@@ -58,12 +58,12 @@ def test_calculate_market_equity():
     output = crsp2[["permco", "mthcaldt", "me"]].head().to_string(index=False)
     # print(output)
     expected = """
-    permco   mthcaldt           me
-    7952 1986-01-31 16100.000000
-    7952 1986-02-28 11960.000000
-    7952 1986-03-31 16330.000000
-    7952 1986-04-30 15172.000000
-    7952 1986-05-30 11793.859375
+    permco   mthcaldt         me
+    7952 1986-01-31 16100.0000
+    7952 1986-02-28 11960.0000
+    7952 1986-03-31 16330.0000
+    7952 1986-04-30 15172.0000
+    7952 1986-05-30 11793.9542
     """
     assert output.replace(" ", "").replace("\n", "") == expected.replace(" ", "").replace("\n", "")
 
@@ -71,14 +71,14 @@ def test_factors():
     vwret, vwret_n, ff_factors, ff_nfirms = create_Fama_French_factors(data_dir=DATA_DIR)
     expected = """
         date        BH        BL        BM
-    2022-08-31 -0.014872 -0.046368 -0.018841
-    2022-09-30 -0.087952 -0.088352 -0.094143
-    2022-10-31  0.147654  0.059447  0.110792
-    2022-11-30  0.049567  0.048842  0.055844
+    2022-08-31 -0.014792 -0.046368 -0.018841
+    2022-09-30 -0.087951 -0.088352 -0.094143
+    2022-10-31  0.147658  0.059447  0.110792
+    2022-11-30  0.049565  0.048842  0.055844
     2022-12-31 -0.049520 -0.067278 -0.043296
     """.replace(" ", "").replace("\n", "")
-    output = ff_factors[['date', 'BH', 'BL', 'BM']].tail().to_string(index=False).replace(" ", "").replace("\n", "")
+    output = ff_factors[['date', 'BH', 'BL', 'BM']].tail().to_string(index=False)
     # print(output)
-    assert expected == output
+    assert expected == output.replace(" ", "").replace("\n", "")
 
     
